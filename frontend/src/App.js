fb0f497a1d13d09a13a1d5a659c3b6d5039c4a9e
offiza <matveygorelik070118@gmail.com>
2022-04-06 14:52:41 +0300

    Add NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { HomePage } from "./HomePage/HomePage.tsx";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AuthPage } from "./AuthPage/AuthPage.tsx";
+import { NavBar } from './NavBar/NavBar.tsx';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="*" element={<Navigate to="/auth" />} />
         <Route path="/home" element={<HomePage />} />
---
+++ frontend/src/NavBar/NavBar.tsx
@@ -0,0 +1,25 @@
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const NavBar = () => {
+  return (
+    <AppBar position="static">
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', p: '10px' }}>
+        <Link to="/home" style={{ textDecoration: 'none', color: '#fff', width: '200px', textAlign: 'center' }}>
+          <Typography variant="h6" color="inherit" component="div">
+            Tasks
+          </Typography>
+        </Link>
+        <Typography variant="h6" color="inherit" component="div" sx={{ width: '200px', textAlign: 'center' }}>
+          Todo List By Offiza
+        </Typography>
+        <Link to="/auth" style={{ textDecoration: 'none', color: '#fff', width: '200px', textAlign: 'center' }}>
+          <Typography variant="h6" color="inherit" component="div">
+            Login/register
+          </Typography>
+        </Link>
+      </Box>
+    </AppBar>
+  )
+}
